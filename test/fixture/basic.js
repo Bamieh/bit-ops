a186701ff5b9447351491205e2fcd721a239f6d2
Bamieh <ahmadbamieh@gmail.com>
2017-11-02 13:04:29 +0200

    bootstrapping code

---
+++ test/fixture/basic.js
@@ -0,0 +1,16 @@
+export const CANDIDATES = [
+  {paymentTypes: ['prepaid'], benefits: ['C', 'A'], stars: 4},
+  {paymentTypes: ['cardless'], benefits: ['A', 'B'], stars: 4},
+  {paymentTypes: ['prepaid'], benefits: ['C', 'B'], stars: 5},
+  {paymentTypes: ['postpaid'], benefits: ['A', 'C'], stars: 3},
+  {paymentTypes: ['postpaid'], benefits: ['B'], stars: 2},
+  {paymentTypes: ['prepaid'], benefits: ['A', 'C'], stars: 3},
+  {paymentTypes: ['prepaid'], benefits: ['B'], stars: 5},
+  {paymentTypes: ['cardless'], benefits: ['A'], stars: 5},
+];
+
+export const FILTERS = {
+  paymentTypes: ['prepaid', 'cardless', 'postpaid'],
+  benefits: ['A', 'B', 'C'],
+  stars: [1,2,3,4,5],
+}
